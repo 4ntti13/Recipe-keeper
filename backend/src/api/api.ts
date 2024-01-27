7e51644f397bd24ce4999c67592c402c2485564a
AnttiN <antti.narvanen86@gmail.com>
2024-01-27 23:38:12 +0200

    get, post and delete works, next todo: patch

--- backend/src/api/api.ts
+++ backend/src/api/api.ts
@@ -1,7 +1,5 @@
-// api.ts:
-
-import express, {Request, Response, Router} from 'express';
-import pool from '../db/db'
+import express, { Request, Response, Router } from 'express';
+import pool from '../db/db';  
 
 const router: Router = express.Router();
 
@@ -12,11 +10,49 @@ router.get('/recipes', async (req: Request, res: Response) => {
         const result = await pool.query('SELECT * FROM recipes');
         res.json(result.rows);
     } catch (error) {
+        console.error(error);  
         res.status(500).json({ message: 'Virhe haettaessa reseptejä' });
+    }
+});
+
+
+router.post('/recipes', async (req: Request, res: Response) => {
+    try {
+        const { name, picture_url, guide, review } = req.body;  
+        const query = `
+            INSERT INTO recipes (name, picture_url, guide, review)
+            VALUES ($1, $2, $3, $4)
+            RETURNING *;`;
+        
+        const values = [name, picture_url, guide, review];
+
+        const result = await pool.query(query, values);
+
+        res.status(201).json(result.rows[0]);
+
+    } catch (error) {
+         console.error(error);  
+         res.status(500).json({ message: 'Virhe reseptin lisäämisessä'});
+    }
+});
+
+router.delete('/recipes/:id', async (req: Request, res: Response) => {
+    try {
+        const id = parseInt(req.params.id);
+        const deleteQuery = 'DELETE FROM recipes WHERE id = $1 RETURNING *;'
+
+        const result = await pool.query(deleteQuery, [id]);
+
+        if (result.rowCount === 0) {
+            return res.status(404).json({ message: 'Reseptiä ei löydy!' });
+        }
+
+        res.status(200).json({ message: 'Resepti poistettu onnistuneesti!', deletedRecipe: result.rows[0] });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Reseptiä ei voitu poistaa!' });
     }
 })
 
-
-
-
-export default Router;
+export default router;  
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import router from './api/api'
 
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use('/api', router);
+
 
 app.get('/', (req, res) => {
     res.send('Works works?!');
