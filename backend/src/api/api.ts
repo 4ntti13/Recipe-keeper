9d54bdb11a33e680b7e1c48e9908e9986fc62c48
AnttiN <antti.narvanen86@gmail.com>
2024-01-28 13:26:17 +0200

    frontend started

--- backend/src/api/api.ts
+++ backend/src/api/api.ts
@@ -4,6 +4,7 @@ import pool from '../db/db';
 const router: Router = express.Router();
 
 
+// READ (all):
 
 router.get('/recipes', async (req: Request, res: Response) => {
     try {
@@ -15,6 +16,7 @@ router.get('/recipes', async (req: Request, res: Response) => {
     }
 });
 
+// CREATE:
 
 router.post('/recipes', async (req: Request, res: Response) => {
     try {
@@ -36,10 +38,12 @@ router.post('/recipes', async (req: Request, res: Response) => {
     }
 });
 
+// DELETE:
+
 router.delete('/recipes/:id', async (req: Request, res: Response) => {
     try {
         const id = parseInt(req.params.id);
-        const deleteQuery = 'DELETE FROM recipes WHERE id = $1 RETURNING *;'
+        const deleteQuery = 'DELETE FROM recipes WHERE id = $1 RETURNING *;';
 
         const result = await pool.query(deleteQuery, [id]);
 
@@ -54,5 +58,60 @@ router.delete('/recipes/:id', async (req: Request, res: Response) => {
         res.status(500).json({ message: 'Reseptiä ei voitu poistaa!' });
     }
 })
+
+// UPDATE:
+
+router.patch('/recipes/:id', async (req: Request, res: Response) => {
+    try {
+        const id = parseInt(req.params.id);
+        const { name, picture_url, guide, review } = req.body;
+
+        const values = [];
+        const updates = [];
+        
+        if (name) {
+            values.push(name);
+            updates.push(`name = $${values.length}`);
+        }
+        if (picture_url) {
+            values.push(picture_url)
+            updates.push(`picture_url = $${values.length}`);
+        }
+        if (guide) {
+            values.push(guide);
+            updates.push(`guide = $${values.length}`);
+        }
+        if (review !== undefined) {
+            values.push(review);
+            updates.push(`review = $${values.length}`);
+        }
+
+        if (updates.length === 0) {
+            return res.status(400).json({ message: 'Ei päivitettäviä tietoja!' });
+        }
+
+        const query = `
+            UPDATE recipes
+            SET ${updates.join(', ')}
+            WHERE id = $${updates.length + 1}
+            RETURNING *;`;
+
+        values.push(id);
+
+        const result = await pool.query(query, values);
+
+        if (result.rowCount === 0) {
+            return res.status(200).json({ message: 'Reseptiä ei löydy!' });
+        }
+
+        res.status(200).json(result.rows[0]);
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Virhe päivittäessä reseptiä!' });
+    }
+})
+
+
 
 export default router;  
